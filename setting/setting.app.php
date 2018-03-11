e4c7d4b5a04afb9314db6bd775315b191b5233e7
zhanjh <zhanjh@126.com>
2018-03-11 15:47:04 +0800

    buildApp gap/project

--- setting/setting.app.php
+++ setting/setting.app.php
@@ -1,7 +1,9 @@
 <?php
 $collection = new \Gap\Config\ConfigCollection();
-
-$collection->set('app', [
-]);
-
+$collection
+    ->set("app", [
+        "Gap\Project" => [
+            "dir" => "app/gap/project",
+        ],
+    ]);
 return $collection;
